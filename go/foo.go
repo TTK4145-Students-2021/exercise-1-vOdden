920a7cf048ae1422a0d24552d9e541de20eb625b
Sigurd Odden <sigurdodden@gmail.com>
2021-01-28 11:15:01 +0100

    .go task updated

--- go/foo.go
+++ go/foo.go
@@ -23,14 +23,13 @@ func decrementing() {
 }
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())    // I guess this is a hint to what GOMAXPROCS does...
-	                                    // Try doing the exercise both with and without it!
+    runtime.GOMAXPROCS(runtime.NumCPU())
+
 
 
 	   go incrementing()
      go decrementing()
-    // We have no way to wait for the completion of a goroutine (without additional syncronization of some sort)
-    // We'll come back to using channels in Exercise 2. For now: Sleep.
+
     time.Sleep(500*time.Millisecond)
     Println("The magic number is:", i)
 }
